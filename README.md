82211ac465d6fd45ac08424097c9e3e4e1f0c471
vtrsg <vittorioschmittg@gmail.com>
2022-09-21 14:30:42 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # canil-node_ts
+
+Projeto feito estudando node + typescript
+
+### Pré-requisitos globais 
+`npm install -g nodemon typescript ts-node`
+
+### Instalação 
+`npm install`
+
+### Para rodar o projeto 
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+//importando bibliotecas necessárias 
+import express  from "express";
+import dotenv from 'dotenv';
+import mustache from "mustache-express";
+// para criar biblioteca publica importando o path 
+import path from "path";
+// configurando as rotas no servidor 
+import mainRoutes from './routes/index'
+
+
+
+//configurando a porta 
+dotenv.config();
+
+//servidor 
+const server = express();
+
+//mustache_template-engine 
+server.set('view engine','mustache');
+//pastas de visualização do projeto 
+server.set('views', path.join(__dirname,'views'));
+server.engine('mustache', mustache());
+
+//configurando pastas publicas__cofigurando pasta de arquivos estaticos 
+server.use(express.static(path.join(__dirname, '../public')));
+
+//rotas 
+server.use(mainRoutes);
+
+server.use((req, res)=>{
+    res.send('página não encontrada');
+});
+
+//rodando servidor process.env.PORT
+server.listen(process.env.PORT);
